7f4252c4d2598bc1b358e0b5a2e5e853629f0411
Matt Bauer <bauer@sitespark.com>
2012-08-15 15:00:59 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewtime::Application.config.secret_token = 'f0295cc6635a07084f8524c9cedcc7bf8f68cf193a1ab29b1d55e4358c919c0a924931d515cdb3bdb3a825fafb5f4a6dec405cb4a7cefad69e6f75c11da6e2c1'
