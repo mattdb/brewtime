bd74b7ff6726a9e02eb58d0fac77c219bedb6a3b
Matt Bauer <bauer@sitespark.com>
2012-08-22 13:09:43 -0500

    Brews controller scaffold

---
+++ app/assets/javascripts/brewtime.js.coffee
@@ -0,0 +1,5 @@
+$(document).ready ->
+  $.datepicker.setDefaults {
+    dateFormat: "yy-mm-dd"
+  }
+  $(".datepicker").datepicker()
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Brewtime::Application.routes.draw do
+  resources :brews do
+    resources :steps
+  end
+  
+  root :to => 'brews#index'
 end
